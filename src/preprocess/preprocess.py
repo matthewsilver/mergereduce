f96b4b91778914abbf9c658484d77639fe01214a
Ubuntu <ubuntu@ip-10-0-0-7.ec2.internal>
2019-06-24 23:26:38 +0000

    fixed error with minhash table after refactoring

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -8,13 +8,13 @@ import redis
 import mmh3
 import numpy as np
 import pickle
+import pyspark.sql.functions as F
 from termcolor import colored
 
 from pyspark.ml.feature import StopWordsRemover
 from pyspark.ml.feature import Tokenizer
 
 from pyspark.sql.types import ArrayType, StringType, IntegerType
-from pyspark.sql.functions import udf, concat, col, lit, explode, count, collect_list, size
 
 from pyspark.conf import SparkConf
 from pyspark.context import SparkContext
@@ -50,7 +50,7 @@ def get_n_gram_shingles(tokens, n):
 
 def to_str(l):
     return str(l)
-to_str_udf = udf(to_str, StringType())
+to_str_udf = F.udf(to_str, StringType())
 
 # Preprocess the data and save to Redis
 def preprocess_files(bucket_name, file_name):
@@ -59,10 +59,10 @@ def preprocess_files(bucket_name, file_name):
 
     # Clean article text
     print(colored("[PROCESSING]: Cleaning article text", "green"))
-    clean_body = udf(lambda body: filter_body(body), StringType())
+    clean_body = F.udf(lambda body: filter_body(body), StringType())
     clean_article_data = raw_data.withColumn("cleaned_body", clean_body("text"))
     # Tokenize article text
-    if (config.LOG_DEBUG): print(colored("[PROCESSING]: Tokenizing text vector...", "green"))
+    print(colored("[PROCESSING]: Tokenizing text vector...", "green"))
     tokenizer = Tokenizer(inputCol="cleaned_body", outputCol="text_body_tokenized")
     tokenized_data = tokenizer.transform(clean_article_data)
     
@@ -73,19 +73,18 @@ def preprocess_files(bucket_name, file_name):
     
     # Stem words
     print(colored("Stemming tokenized text", "green"))
-    stem = udf(lambda tokens: lemmatize(tokens), ArrayType(StringType()))
+    stem = F.udf(lambda tokens: lemmatize(tokens), ArrayType(StringType()))
     stemmed_data = stop_words_removed_data.withColumn("text_body_stemmed", stem("text_body_stop_words_removed"))
     
     # Shingle resulting body
     print(colored("Shingling resulting text", "green"))
-    shingle = udf(lambda tokens: get_n_gram_shingles(tokens, 3), StringType())
+    shingle = F.udf(lambda tokens: get_n_gram_shingles(tokens, 3), StringType())
     shingled_data = stemmed_data.withColumn("text_body_shingled", shingle("text_body_stemmed"))
     shingle_table = shingled_data.select('id', 'text_body_shingled')
-    
-    if (config.LOG_DEBUG): print(colored("Adding category/id mappings to Redis", "green"))
+    print(colored("Adding category/id mappings to Redis", "green"))
  
     # Create a mapping of article categories to article id's that fall under that category. Each key is an article category and the values the list of article id's.
-    cat_id_map = raw_data.select(explode('categories').alias('category'), 'id').groupBy(col('category')).agg(collect_list('id').alias('ids_list')).where(size(col('ids_list')) < 2000).withColumn('ids', to_str_udf('ids_list'))      
+    cat_id_map = raw_data.select(F.explode('categories').alias('category'), 'id').groupBy(F.col('category')).agg(F.collect_list('id').alias('ids_list')).where(F.size(F.col('ids_list')) < 200).withColumn('ids', to_str_udf('ids_list'))      
     print(colored("Beginning writing category/id mapping to Redis", "green")) 
     def write_cat_id_map_to_redis(rdd): 
         rdb = redis.StrictRedis(config.REDIS_SERVER, port=6379, db=0)
@@ -96,17 +95,17 @@ def preprocess_files(bucket_name, file_name):
     print(colored("Finished writing category/id mapping to Redis", "green"))
 
     #Minhash calculations
-    k = 10
+    k = 100
     random_seed = 50
     masks = (np.random.RandomState(seed=random_seed).randint(np.iinfo(np.int64).min, np.iinfo(np.int64).max, k))
 
     def update_min_hash_signature(word, min_hash_signature):
-        root_hash = mmh3.hash64(word)[0]
+        root_hash = mmh3.hash64(pickle.dumps(word))[0]
         word_hashes = np.bitwise_xor(masks, root_hash)  # XOR root hash with k randomly generated integers to simulate k hash functions
         min_hash_signature = np.minimum(min_hash_signature, word_hashes)
         return min_hash_signature
 
-    def calc_min_hash(tokens):
+    def calc_min_hash_signature(tokens):
         min_hash_signature = np.empty(k, dtype=np.int64)
         min_hash_signature.fill(np.iinfo(np.int64).max)
         for token in tokens:
@@ -114,8 +113,8 @@ def preprocess_files(bucket_name, file_name):
         return min_hash_signature
 
     def compute_minhash(df): 
-       calc_min_hash = udf(lambda x: str(list(map(lambda x: int(x), calc_min_hash(x)))), StringType()) 
-       df = df.withColumn("min_hash", calc_min_hash("text_body_shingled")).select('id', 'min_hash') 
+       calc_min_hash_udf = F.udf(lambda x: str(list(map(lambda x: int(x), calc_min_hash_signature(x)))), StringType()) 
+       df = df.withColumn("min_hash", calc_min_hash_udf("text_body_shingled")).select('id', 'min_hash') 
        return df 
     print(colored("Computing minhash values", "green"))
     minhash_df = compute_minhash(shingle_table)
@@ -124,36 +123,22 @@ def preprocess_files(bucket_name, file_name):
     
     # Write minhash data to redis. If pipeline=True, use pipeline
     # method of inserting data in Redis
-    def write_minhash_data_to_redis(rdd, pipeline=False):
+    def write_minhash_data_to_redis(rdd):
         rdb = redis.StrictRedis(config.REDIS_SERVER, port=6379, db=0)
-        if not pipeline:
-            for row in rdd:
-                rdb.sadd('id:{}'.format(row.id), row.min_hash)
-            return
-        else:
-            i = 1
-            pipe = rdb.pipeline()
-            for row in rdd:
-                pipe.set('id:{}'.format(row.id), row.min_hash)
-                i += 1
-                if i % 1000 == 0:
-                    print('adding new batch. at {} entries total'.format(i))
-                    pipe.execute()
-                    pipe = rdb.pipeline()
-    print(minhash_df.show(5, True)) 
+        for row in rdd:
+            print('row: {}'.format(row))
+            print('row_id: {}, row_min_hash: {}'.format(row.id, row.min_hash))
+            rdb.sadd('id:{}'.format(row.id), row.min_hash)
+    #print(minhash_df.show(5, True)) 
     minhash_df.foreachPartition(write_minhash_data_to_redis)
     
     print(colored("Finished writing minhash data to Redis", "green"))
     
-    if (config.LOG_DEBUG): print(colored("[UPLOAD]: Writing preprocessed data to database...", "green"))
+    print(colored("[UPLOAD]: Writing preprocessed data to database...", "green"))
 #    write_aws_s3(config.S3_BUCKET, config.S3_FOLDER_PREPROCESSED, shingled_data)
     cf = configparser.ConfigParser()
     cf.read('../config/db_properties.ini')
     
-def preprocess_all():
-    bucket = util.get_bucket(config.S3_BUCKET)
-    preprocess_files(config.S3_BUCKET, config.S3_FOLDER_EXTRACTED)
-
 def main():
     #spark_conf = SparkConf().setAppName("Text Preprocesser").set("spark.cores.max", "30")
 
@@ -171,7 +156,7 @@ def main():
     sql_context = SQLContext(sc)
 
     start_time = time.time()
-    preprocess_all()
+    preprocess_files(config.S3_BUCKET, config.S3_FOLDER_EXTRACTED)
     end_time = time.time()
     print(colored("Preprocessing run time (seconds): {0}".format(end_time - start_time), "magenta"))
 
